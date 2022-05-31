3fdc6e7b28007d8e9730d6347394f6ef1add317d
kavin033 <rk.kavin2003@gmail.com>
2022-05-31 22:04:28 +0530

    init

--- init.sh
+++ init.sh
@@ -6,5 +6,5 @@ mv DeltaTask1_SysAd/* .
 mv 'NormalUser Mode'/* .
 mv 'SuperUser Mode'/* .
 rm -rf 'NormalUser Mode'   README.md  'SuperUser Mode' DeltaTask1_SysAd
-chmod 770 ${file}/alias.sh ${file}/allotInterest.sh ${file}/genUser.sh ${file}/genSummary.sh ${file}/init.sh ${file}/permit.sh ${file}/updateBranch.sh ${file}/src/summary.txt ${file}/src
+chmod 770 ${file}/aliases.sh ${file}/allotInterest.sh ${file}/genUser.sh ${file}/genSummary.sh ${file}/init.sh ${file}/permit.sh ${file}/updateBranch.sh ${file}/src/summary.txt ${file}/src
 chmod 777 ${file}/makeTransaction.sh
