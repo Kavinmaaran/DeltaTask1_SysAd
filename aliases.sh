beb7ba49fb9212600b210810c4453ab815876059
Kavinmaaran R K <94862703+kavin033@users.noreply.github.com>
2022-06-02 00:02:04 +0530

    aliases updated for users

--- aliases.sh
+++ aliases.sh
@@ -15,6 +15,23 @@ echo "alias makeTransaction='/home/CEO/makeTransaction.sh'" >> /home/CEO/.bashrc
 echo "alias genSummary='/home/CEO/genSummary.sh'" >> /home/CEO/.bashrc
 source /home/CEO/.bashrc
 
+
+for j in $(ls -d Branch*)
+do
+    cd $j
+    for i in $(ls -d ACC*)
+    do
+    	echo "alias makeTransaction='/home/CEO/makeTransaction.sh'" >> /home/CEO/$j/$i/.bashrc
+    	source /home/CEO/$j/$i/.bashrc
+    done
+    echo "alias updateBranch='/home/CEO/updateBranch.sh'" >> /home/CEO/$j/.bashrc
+    echo "alias allotInterest='/home/CEO/allotInterest.sh'" >> /home/CEO/$j/.bashrc
+    echo "alias makeTransaction='/home/CEO/makeTransaction.sh'" >> /home/CEO/$j/.bashrc
+    echo "alias genSummary='/home/CEO/genSummary.sh'" >> /home/CEO/$j/.bashrc
+    cd ..
+    source /home/CEO/$j/.bashrc
+done
+
 crontab -l > cron
 echo "* * * * *  ${file}/allotInterest.sh" >>cron
 crontab cron
