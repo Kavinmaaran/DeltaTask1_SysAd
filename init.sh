a869e10e8407b56d21c7dc3770c87b64b6cfe5e4
kavin033 <rk.kavin2003@gmail.com>
2022-06-03 09:55:16 +0530

    genSummary

--- init.sh
+++ init.sh
@@ -6,6 +6,6 @@ mv DeltaTask1_SysAd/* .
 mv 'NormalUser Mode'/* .
 mv 'SuperUser Mode'/* .
 rm -rf 'NormalUser Mode'   README.md  'SuperUser Mode' DeltaTask1_SysAd
-chmod 770 ${file}/allotInterest.sh ${file}/genSummary.sh ${file}/updateBranch.sh ${file}/src/summary.txt ${file}/src
+chmod 770 ${file}/allotInterest.sh ${file}/genSummary.sh ${file}/updateBranch.sh ${file}/src/summary.txt ${file}/src/mean.txt ${file}/src/meanusr.txt ${file}/src/mode.txt ${file}/src/modeusr.txt ${file}/src/median.txt ${file}/src/medianusr.txt
 chmod 700 ${file}/aliases.sh ${file}/genUser.sh ${file}/init.sh ${file}/permit.sh 
 chmod 777 ${file}/makeTransaction.sh
